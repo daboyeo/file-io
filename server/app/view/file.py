35c8e520c311cf3f87fa2ba1c858e0d739f1177b
Minki Son <migsking@naver.com>
2021-04-27 16:05:41 +0900

    Update file.py

--- server/app/view/file.py
+++ server/app/view/file.py
@@ -11,7 +11,6 @@ class File(Resource):
 
     def get(self):
         uuid = request.args.get("uuid")
-        print(uuid)
         return send_from_directory(directory=File.image_path, filename=uuid)
 
     def post(self):
@@ -25,4 +24,4 @@ class File(Resource):
             return {'message': 'unsupported extension name'}, 400
 
         file.save(os.path.join(File.image_path, uuid+file_name))
-        return {'file_uuid': uuid}, 201
+        return {'file_uuid': uuid+file_name}, 201
